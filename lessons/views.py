39669b85383b1761a9652e9cd96e327145f1501b
Deys Timofey <nxexox@gmail.com>
2017-02-01 01:45:07 +0500

    init

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello_world_view(request):
+    """ Стандартный ответ на запрос. Возвращается со статусом 200 """
+    return HttpResponse()
+
+
+def hello_world_string(request):
+    """ Передача данных в теле ответа """
+    return HttpResponse("Hello World")
+
+
+def hello_world_403(request):
+    """ Передача данных, и установка своего кода ответа. """
+    return HttpResponse("Hello world", status=403)
--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from lessons import views
+
+
 urlpatterns = [
+    url(r'^$', 'lessons.views.hello_world_view'),
+    url(r'^string/$', views.hello_world_string),
+    url(r'^string/403/$', views.hello_world_403),
     url(r'^admin/', admin.site.urls),
 ]

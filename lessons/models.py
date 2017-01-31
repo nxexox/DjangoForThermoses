58799c484a76b57560f2781d6491767d071fcf86
Deys Timofey <nxexox@gmail.com>
2017-02-01 03:30:33 +0500

    initial

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Lesson(models.Model):
+    test = models.FloatField()  # Число с плавающей точкой
+    name = models.CharField("Название", max_length=150)  # Строка максимальной длины 150 символов
+    count = models.IntegerField("Кол-во", default=10)  # Целое число
+    preview = models.TextField("Превью", null=True, blank=True, default='')  # Неограниченный текст
+    date_create = models.DateField("Дата создания", auto_now=True)  # Дата
+    date_update = models.DateTimeField("Дата последнего изменения", auto_now=True)  # Дата и время
+    is_true = models.BooleanField("Это правда?", default=False)  # Булевое значение
+
+    class Meta:
+        verbose_name = "Урок 5"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return "{} {}".format(self.name, self.count)
